7def14ad670d26bd313d4171fd071fa889f8d9b6
Dominik Jones <dominikjo22@gmail.com>
2023-01-24 12:20:11 -0500

    update

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ dotenv.config()
 const salt = bcrypt.genSaltSync(10);
 const secret = process.env.SECRET_KEY;
 
-app.use(cors({credentials:true,origin:'https://blog-hog.onrender.com'}));
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
